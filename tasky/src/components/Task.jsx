fc0508c1e44ec35bac0f98e231cf6b6c821388a8
leon .... <20103258@mail.wit.ie>
2025-01-29 18:02:44 +0000

    tasky lab 2a- excercise done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Task = (props) => {
     
+    
+
     return (
         <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="task-description">{props.description}</p>
+            <p className={`priority-${props.priority}`}>Priority: {props.priority}</p>
+
         </div>
     )
 
