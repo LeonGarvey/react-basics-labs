d06de707c72ad0ac4cf9d6a4d706da65a22d2457
leon .... <20103258@mail.wit.ie>
2025-02-05 17:13:04 +0000

    Delete a task

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -5,13 +5,17 @@ const Task = (props) => {
     
 
     return (
-        <div className="card">
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="task-description">{props.description}</p>
             <p className={`priority-${props.priority}`}>Priority: {props.priority}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
 
         </div>
+        
     )
 
 
